286874d14de0ec30222633c8898a56bd8886c755
Melvin Idema <melvin_i13@hotmail.com>
2022-03-11 17:11:31 +0100

    Add random dogs to homepage

--- app/controllers/AppController.js
+++ app/controllers/AppController.js
@@ -1,8 +1,22 @@
 import User from '../models/User.js';
+import Log from '../services/Log.js';
+import fetch from 'node-fetch';
+import dogNames from 'dog-names';
 
 async function show(req, res) {
   const user = await User.getByEmail(req.session.email);
-  const allUsers = await User.getAll();
+  let dogs = [];
+
+  try {
+    const dogsApi = await fetch('https://dog.ceo/api/breeds/image/random/5');
+    const dogImages = await dogsApi.json();
+    dogs = dogImages.message.map((dog) => ({
+      img: dog,
+      name: dogNames.allRandom(),
+    }));
+  } catch (err) {
+    Log(err);
+  }
 
   res.render('index', {
     user: {
@@ -10,18 +24,7 @@ async function show(req, res) {
       name: user.name,
       avatar: user.avatar,
     },
-    users: allUsers,
-    dogs: [
-      { name: 'diederik' },
-      { name: 'jamie' },
-      { name: 'johan' },
-      { name: 'kieran' },
-      { name: 'richard' },
-    ],
-    alert: {
-      title: 'Under Construction',
-      body: 'This site is under construction and might not work properly.',
-    },
+    dogs: dogs,
   });
 }
 
